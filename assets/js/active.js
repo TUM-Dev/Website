589668d02d5222d87288b9896603555529f3e36d
Kordian Bruck <k@bruck.me>
2018-07-24 13:20:38 +0200

    Add template

---
+++ assets/js/active.js
@@ -0,0 +1,82 @@
+(function ($) {
+    'use strict';
+
+    // [ JS Active Code Index ]
+
+    // :: 1.0 Owl Carousel Active Code
+    // :: 2.0 Slick Active Code
+    // :: 3.0 Footer Reveal Active Code
+    // :: 4.0 ScrollUp Active Code
+    // :: 5.0 CounterUp Active Code
+    // :: 6.0 onePageNav Active Code
+    // :: 7.0 Magnific-popup Video Active Code
+    // :: 8.0 Sticky Active Code
+    // :: 9.0 Preloader Active code
+
+    // :: 1.0 Owl Carousel Active Code
+    if ($.fn.owlCarousel) {
+        $(".welcome_slides").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            smartSpeed: 1500,
+            nav: true,
+            navText: ["<i class='pe-7s-angle-left'</i>", "<i class='pe-7s-angle-right'</i>"]
+        });
+        $(".app_screenshots_slides").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            smartSpeed: 800,
+            margin: 30,
+            center: true,
+            dots: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 3
+                },
+                992: {
+                    items: 5
+                }
+            }
+        });
+    }
+
+    // :: 3.0 Footer Reveal Active Code
+    if ($.fn.footerReveal) {
+        $('footer').footerReveal({
+            shadow: true,
+            shadowOpacity: 0.3,
+            zIndex: -101
+        });
+    }
+
+    // :: 4.0 ScrollUp Active Code
+    $('#scrollUp').on('click', function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    })
+
+    // :: 6.0 onePageNav Active Code
+    if ($.fn.onePageNav) {
+        $('#nav').onePageNav({
+            currentClass: 'active',
+            scrollSpeed: 2000,
+            easing: 'easeOutQuad'
+        });
+    }
+
+    // Show fixed bar & scrollTop only after scrolling down a bit
+    var $window = $(window);
+    $window.on('scroll', function () {
+        if ($window.scrollTop() > 48) {
+            $('.header_area').addClass('sticky slideInDown');
+            $('#scrollUp').fadeIn('fast');
+        } else {
+            $('.header_area').removeClass('sticky slideInDown');
+            $('#scrollUp').fadeOut('fast');
+        }
+    });
+})(jQuery);
