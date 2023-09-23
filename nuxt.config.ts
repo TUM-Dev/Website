f796b7e00df91cba6e017f2f0084da021171deb7
Frank Elsinga <frank@elsinga.de>
2023-09-23 23:47:20 +0200

    ran the linter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss','@nuxtjs/color-mode'],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
   devtools: { enabled: true },
   colorMode: {
-    preference: 'system', // default theme
-    dataValue: 'theme', // activate data-theme in <html> tag
-    classSuffix: '',
+    preference: "system", // default theme
+    dataValue: "theme", // activate data-theme in <html> tag
+    classSuffix: "",
   },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: [require('daisyui')],
+  plugins: [require("daisyui")],
   daisyui: {
     themes: ["light", "dark", "cyberpunk"],
   },
