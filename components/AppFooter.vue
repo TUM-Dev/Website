0a914af0e17b0dd2f851aee2df4b670b9562c6b1
Frank Elsinga <frank@elsinga.de>
2023-09-23 23:47:34 +0200

    switched to using components ^^

---
+++ components/AppFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer class="footer p-10 bg-base-200 text-base-content">
+  <aside>
+    <Logo/>
+    <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
+  </aside> 
+  <nav>
+    <header class="footer-title">Services</header> 
+    <a class="link link-hover">Branding</a> 
+    <a class="link link-hover">Design</a> 
+    <a class="link link-hover">Marketing</a> 
+    <a class="link link-hover">Advertisement</a>
+  </nav> 
+  <nav>
+    <header class="footer-title">Company</header> 
+    <a class="link link-hover">About us</a> 
+    <a class="link link-hover">Contact</a> 
+    <a class="link link-hover">Jobs</a> 
+    <a class="link link-hover">Press kit</a>
+  </nav> 
+  <nav>
+    <header class="footer-title">Legal</header> 
+    <a class="link link-hover">Terms of use</a> 
+    <a class="link link-hover">Privacy policy</a> 
+    <a class="link link-hover">Cookie policy</a>
+  </nav>
+</footer>
+</template>
---
+++ components/AppHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="navbar bg-base-300">
+    <div class="flex-1">
+      <a class="btn btn-ghost p-2">
+        <Logo/>
+      </a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#team">Team</a></li>
+        <li><a href="#join-us">Join us</a></li>
+        <li>
+          <details>
+            <summary>Theme</summary>
+            <ul class="p-2 bg-base-100">
+              <li v-for="col of colors" v-key="col">
+                <a @click="colorMode.preference = col">{{ col }}</a>
+              </li>
+            </ul>
+          </details>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const colorMode = useColorMode();
+const colors = ["system", "light", "dark", "cyberpunk"];
+</script>
