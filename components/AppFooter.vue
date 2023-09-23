77d56bac6897de641898904aefe1b40391f07979
Frank Elsinga <frank@elsinga.de>
2023-09-24 01:57:16 +0200

    added a rudimentary contact form

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,28 +1,16 @@
 <template>
-  <footer class="footer p-10 bg-base-200 text-base-content">
+  <ContactForm class="px-10 pt-5 bg-base-200 text-base-content" />
+  <footer class="footer footer-center p-10 bg-base-200 text-base-content">
     <aside>
-      <AppLogo />
-      <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+      <AppLogo class="h-20" />
+      <p>Collaborating on Software since 2012</p>
     </aside>
-    <nav>
-      <header class="footer-title">Services</header>
-      <a class="link link-hover">Branding</a>
-      <a class="link link-hover">Design</a>
-      <a class="link link-hover">Marketing</a>
-      <a class="link link-hover">Advertisement</a>
-    </nav>
-    <nav>
-      <header class="footer-title">Company</header>
-      <a class="link link-hover">About us</a>
-      <a class="link link-hover">Contact</a>
-      <a class="link link-hover">Jobs</a>
-      <a class="link link-hover">Press kit</a>
-    </nav>
-    <nav>
-      <header class="footer-title">Legal</header>
-      <a class="link link-hover">Terms of use</a>
+    <nav class="grid grid-flow-col gap-4">
+      <a class="link link-hover">Projects</a> •
+      <a class="link link-hover">Team</a> •
+      <a class="link link-hover">Join us</a> •
+      <a class="link link-hover">Imprint</a> •
       <a class="link link-hover">Privacy policy</a>
-      <a class="link link-hover">Cookie policy</a>
     </nav>
   </footer>
 </template>
---
+++ components/ContactForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="flex flex-col sm:flex-row justify-evenly">
+    <div class="pe-0 sm:pe-5">
+      <header class="footer-title">Contact us</header>
+      <p class="hidden sm:block pb-2">
+        If you would like to contact us, you can use the contact form or send an
+        email to the address below.
+      </p>
+
+      <p>
+        <b>Adresse:</b> ITSZ - OpenSource@TUM, Arcisstraße 21, 80333 München
+      </p>
+      <p><b>E-Mail:</b> app@tum.de</p>
+      <div class="sm:hidden px-3 divider">OR</div>
+    </div>
+    <form>
+      <div class="form-control">
+        <label class="label">
+          <span class="label-text">What should we call you?</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Your name"
+          class="input input-bordered"
+        />
+      </div>
+      <div class="form-control">
+        <label class="label">
+          <span class="label-text">
+            If we have further questions, how can we get in touch?
+          </span>
+        </label>
+        <input
+          type="text"
+          placeholder="firstname.secondname@tum.de"
+          class="input input-bordered"
+        />
+      </div>
+      <div class="form-control">
+        <label class="label">
+          <span class="label-text">Your message</span>
+        </label>
+        <textarea class="textarea textarea-bordered h-24"></textarea>
+      </div>
+      <div class="form-control py-3">
+        <button class="btn btn-primary">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
